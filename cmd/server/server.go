b836bcaf712db37aae6c5c92c44fa539816a4237
Andrée Björkgård <andree@bjorkgard.se>
2024-02-10 23:41:24 +0100

    ignoring bluez logs that arent errors

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -6,10 +6,12 @@ import (
 	"github.com/andree-bjorkgard/remote-bluetooth/internal/bluetooth"
 	"github.com/andree-bjorkgard/remote-bluetooth/internal/config"
 	"github.com/andree-bjorkgard/remote-bluetooth/internal/discovery"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	cfg := config.NewConfig()
+	logrus.SetLevel(logrus.ErrorLevel)
 
 	discoveryService := discovery.NewDiscoveryService(cfg.BroadcastPort, cfg.BroadcastMessage, cfg.BroadcastServerResponse)
 
